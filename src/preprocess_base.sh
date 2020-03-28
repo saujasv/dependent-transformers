faab5cd4970c5bc24234c70483f375d7fe204dc4
Saujas <saujas1999@gmail.com>
2020-03-28 23:00:37 +0530

    changed preprocessing script

--- src/preprocess_base.sh
+++ src/preprocess_base.sh
@@ -5,9 +5,12 @@ src_lang=$2
 trg_lang=$3
 
 onmt_preprocess \
-    --train_src $1/${src_lang}.txt \
-    --train_tgt $1/${trg_lang}.txt \
+    --train_src $1/${src_lang}_train.txt \
+    --train_tgt $1/${trg_lang}_train.txt \
+    --valid_src $1/${src_lang}_valid.txt \
+    --valid_tgt $1/${trg_lang}_valid.txt \
+    --save_data ${src_lang}_${trg_lang}_data \
     --src_words_min_frequency 2 \
-    --tgt_words_min_frequency 2 \
-    --shuffle \
-    --save_data ${src_lang}_${trg_lang}_data
+    --tgt_words_min_frequency 2
+
+    
