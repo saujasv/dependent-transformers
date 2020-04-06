5a24a940cb388134ba9e6169934999ecb4fccbdd
Ujwal Narayan <31547494+ujwal-narayan@users.noreply.github.com>
2020-04-06 14:10:11 +0530

    Added translate.sh

---
+++ src/translate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LC_ALL=en_US.UTF-8
+cd ~/nlpa/dependent-transformers/src 
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate nla
+
+model_name=$1
+model_lang=$2
+model_preds=$model_name"_preds.txt"
+data_path="./../data/splits/test/"
+output_path="./../predictions/"$model_preds
+
+
+onmt_translate \
+	--model ./../models/$model_name \
+	--beam_size 4 \
+	--alpha 0.6 \
+	--src $data_path"en_test.txt" \
+	--length_penalty avg \
+	--output $output_path 
+
+python get_bleu_score.py $data_path$model_lang"_text.txt" $output_path
