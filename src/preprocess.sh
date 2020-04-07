500b2298a61f15240c9149eeed4f5f51b6b0964b
Saujas <saujas1999@gmail.com>
2020-04-08 00:32:28 +0530

    Incorporated shuffling of training data in preprocessing script

--- src/preprocess.sh
+++ src/preprocess.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-prefix=$1
-src_lang=$2
-trg_lang=$3
+src_train_file=$1
+trg_train_file=$2
+src_valid_file=$3
+trg_valid_file=$4
+data_file_name=$5
+
+paste -d \| $src_train_file $trg_train_file | shuf | awk -v FS='|' '{ print $1 > "dump_src.txt" ; print $2 > "dump_trg.txt" }'
+rm $src_train_file $trg_train_file
+mv dump_src.txt $src_train_file
+mv dump_trg.txt $trg_train_file
 
 onmt_preprocess \
-    --train_src $1/train/${src_lang}_train.txt \
-    --train_tgt $1/train/${trg_lang}_train.txt \
-    --valid_src $1/valid/${src_lang}_valid.txt \
-    --valid_tgt $1/valid/${trg_lang}_valid.txt \
-    --save_data /scratch/${src_lang}_${trg_lang}_data \
+    --train_src $src_train_file \
+    --train_tgt $trg_train_file \
+    --valid_src $src_valid_file \
+    --valid_tgt $trg_valid_file \
+    --save_data $data_file_name \
     --src_words_min_frequency 2 \
     --tgt_words_min_frequency 2
